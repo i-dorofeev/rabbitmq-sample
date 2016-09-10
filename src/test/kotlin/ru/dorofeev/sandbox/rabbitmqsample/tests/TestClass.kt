990a05b12b1f7d5756693e27a47ec25a69f70656
Ilya Dorofeev <i.dorofeev@jet.msk.su>
2016-09-10 19:53:52 +0300

    Initial commit

---
+++ src/test/kotlin/ru/dorofeev/sandbox/rabbitmqsample/tests/TestClass.kt
@@ -0,0 +1,14 @@
+package ru.dorofeev.sandbox.rabbitmqsample.tests
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class TestClass {
+
+	@Test
+	fun testMethod() {
+
+		Assertions.assertEquals(5, 4, "5 != 4")
+
+	}
+}
